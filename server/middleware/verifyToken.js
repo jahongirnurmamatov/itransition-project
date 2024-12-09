23bcec7a374ff1d77ccbc663772d298f22765617
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-09 18:24:36 +0500

    verifytoken middleware

---
+++ server/middleware/verifyToken.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized, no token" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded)
+      return res
+        .status(401)
+        .json({ success: false, message: "Unauthorized, invalid token" });
+    req.user = decoded.userId;
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Unauthorized, token is expired" });
+  }
+};
