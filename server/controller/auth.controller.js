c01cee211fe6b52dafa27bbe0a0fd3f53de33982
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-09 18:11:16 +0500

    login funcitonality

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -24,3 +24,30 @@ export const signup = async(req, res) => {
     return res.status(500).json({ success: false, message: error.message });
   }
 };
+
+export const login = async(req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
+    const isPasswordValid = bcrypt.compareSync(password, user.password);
+    if (!isPasswordValid) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+    generatTokenAndSetCookie(res,user._id); 
+    return res.status(200).json({ success: true, message: "User logged in" });    
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+}
