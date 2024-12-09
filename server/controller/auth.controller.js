80705b6ec697cd6b539368d5e54b7035012a7fb6
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-09 17:43:42 +0500

    user signup

---
+++ server/controller/auth.controller.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
+
+
+export const signup = (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, message: "All fields are required" });
+    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    user.save();
+    return res.status(200).json({ success: true, message: "User created" });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './db/connectDB.js';
+import authRoute from './routes/auth.route.js';
 dotenv.config();
 
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
+
+// routes
+app.use('/api/auth', authRoute);
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend');
