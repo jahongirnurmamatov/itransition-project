832f596f87b0c421cae1fb3c7eaae7762812b844
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-09 16:36:03 +0500

    database setup and connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './db/connectDB.js';
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello from the backend');
 });
 
-app.listen(4000, () => {
-    console.log('Listening on port 4000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    connectDB();
 });
