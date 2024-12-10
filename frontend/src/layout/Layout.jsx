78b73735e7d3611286dd568756da1114dd910283
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-10 16:57:02 +0500

    mui sidebar integration

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,13 +1,97 @@
-import React from "react";
+import * as React from "react";
+import { createTheme } from "@mui/material/styles";
+import { LuCirclePlus } from "react-icons/lu";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import { FaChartPie } from "react-icons/fa";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import DescriptionIcon from "@mui/icons-material/Description";
+import LayersIcon from "@mui/icons-material/Layers";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { DashboardLayout } from "@toolpad/core/DashboardLayout";
+import { useDemoRouter } from "@toolpad/core/internal";
 import { Outlet } from "react-router-dom";
 
-const Layout = () => {
+const NAVIGATION = [
+  {
+    kind: "header",
+    title: "Main items",
+  },
+  {
+    segment: "dashboard",
+    title: "My Templates",
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: "create",
+    title: "Create",
+    icon: <LuCirclePlus size={26}  />,
+  },
+  {
+    kind: "divider",
+  },
+  {
+    kind: "header",
+    title: "Analytics",
+  },
+  {
+    segment: "answers",
+    title: "Reports",
+    icon: <BarChartIcon />,
+    children: [
+      {
+        segment: "sales",
+        title: "Responses",
+        icon: <DescriptionIcon />,
+      },
+      {
+        segment: "traffic",
+        title: "Statistics",
+        icon: <FaChartPie  size={26}  />,
+      },
+    ],
+  },
+  {
+    segment: "integrations",
+    title: "Integrations",
+    icon: <LayersIcon />,
+  },
+];
+
+const demoTheme = createTheme({
+  cssVariables: {
+    colorSchemeSelector: "data-toolpad-color-scheme",
+  },
+  colorSchemes: { light: true, dark: true },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 600,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+});
+
+function Layout(props) {
+  const { window } = props;
+
+  const router = useDemoRouter("/dashboard");
+  const demoWindow = window !== undefined ? window() : undefined;
+
   return (
-    <div>
-      <h1>This is layout</h1>
-      <Outlet />
-    </div>
+    // preview-start
+    <AppProvider
+      navigation={NAVIGATION}
+      router={router}
+      theme={demoTheme}
+      window={demoWindow}
+    >
+      <DashboardLayout>
+        <Outlet />
+      </DashboardLayout>
+    </AppProvider>
   );
-};
+}
 
 export default Layout;
