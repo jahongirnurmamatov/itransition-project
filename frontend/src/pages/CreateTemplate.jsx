a1806a171483e8c3ad839f74181f26a34c5b6ba5
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-13 22:00:24 +0500

    Changing label of form and description

--- frontend/src/pages/CreateTemplate.jsx
+++ frontend/src/pages/CreateTemplate.jsx
@@ -5,24 +5,41 @@ import RadioForm from '@/components/createForm/RadioFrom'
 import SelectForm from '@/components/createForm/SelectForm'
 import { Input } from '@/components/ui/input'
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
 const CreateTemplate = () => {
   const [formType,setFormType]=useState('');
   const [forms, setForms] = useState([]);
+  const [editing,setEditing]=useState(false);
 
    const addForm = (formType)=>{
-    console.log(formType)
     const newForm = {
+      id: uuidv4(),
       type: formType,
       label : 'Untitled label',
       options : [],
       required : false,
-      description : 'Write small description here...',
-      name : '',
-      id : '',
+      description : 'Write small description here...'
     }
     setForms([...forms,newForm])
    }
+   const editLabel = (id, label) => {
+    const allforms = [...forms];
+    const index = forms.findIndex((form) => form.id === id);
+    if(index!==-1){
+      allforms[index].label = label;
+      setForms(allforms);
+    }
+  }
+
+   const editDescription = (id, description) => {
+    const allforms = [...forms];
+    const index = forms.findIndex((form) => form.id === id);
+    if(index!==-1){
+      allforms[index].description = description;
+      setForms(allforms);
+    }
+  }
 
   return (
     <div className="w-full bg-slate-100 min-h-screen flex items-start justify-center">
@@ -32,7 +49,17 @@ const CreateTemplate = () => {
           {
             forms.map((form,index)=>(
               <div key={index} className="w-full bg-white rounded-lg flex flex-col gap-3 px-5 py-3 shadow-md">
-                 <p className='text-lg font-semibold font-sans'>{form.label} </p>
+                {editing ? (
+                  <Input onBlur={()=>setEditing(false)}  type="text" value={form.label} onChange={(e) => editLabel(form.id, e.target.value)} className="w-2/3 p-2" />
+                ) : (
+                 <p onClick={()=>setEditing(true)} onBlur={()=>setEditing(false)} className='text-lg font-semibold font-sans'>{form.label} </p>   
+                )}
+                {editing ? (
+                  <Input onBlur={()=>setEditing(false)}  type="text" value={form.description} onChange={(e) => editDescription(form.id, e.target.value)} className="w-2/3 p-2" />  
+                ) : (
+                  <p onClick={()=>setEditing(true)}   className='text-sm font-light text-gray-500'>{form.description}</p>
+                )}
+
                  {
                    (() => {
                     switch (form.type) {
