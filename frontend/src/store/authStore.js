a147a97d7858a4392b67b636e0e30220edc62c5b
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-15 12:40:06 +0500

    signup function in authStore

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -6,4 +6,15 @@ export const useAuthStore = create((set)=>({
     error:null,
     isLoading:false,
     isCheckingAuth:true,
+
+    signup: async(email,password, name)=>{
+        set({isLoading:true, error:null});
+        try {
+            const res = await axios.post(`/auth/signup`, {email, password, name});
+            set({user:res.data.user, isAuthenticated:true, isLoading:false});
+        } catch (error) {
+            set({error:error.response.data.message, isLoading:false});
+            throw Error(error);
+        }
+    },
 }))
