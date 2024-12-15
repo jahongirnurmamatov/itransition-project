2e83010c01ee3c972f351216d5ad8f5ad7c03ab9
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-15 11:36:01 +0500

    social media icons added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,13 +3,15 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Lock, MailCheck, UserRoundCheck } from 'lucide-react'
 import React, { useState } from 'react'
+import { FaGoogle } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 const Login = () => {
   const [state, setState] = useState('Login');
   const [error,setError] = useState('');
   return (
     <div className='w-screen h-screen bg-slate-200'>
       <div className="flex w-full h-full items-center justify-center">
-        <div className="w-[50%] h-2/3 bg-white flex gap-0 rounded-lg shadow-lg">
+        <div className="w-[50%] h-3/4 bg-white flex gap-0 rounded-lg shadow-lg">
           <div className="flex-1">
             <img className='object-cover w-full h-full' src="https://images.pexels.com/photos/7307532/pexels-photo-7307532.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
           </div>
@@ -46,7 +48,17 @@ const Login = () => {
               }
               {error && <p className='text-red-500 text-sm'>{error}</p>}
               </form>
+              <div className="flex gap-2 w-[70%] items-center justify-center my-3">
+                <div className="bg-gray-300 flex-1 h-[1px]"/>
+                <p className='text-xs text-gray-500'>or</p>
+                <div  className="bg-gray-300 flex-1 h-[1px]"/>
+              </div>
               
+              <div className="flex gap-3">
+                <Button  className='rounded-full p-2 hover:bg-slate-300 hover:scale-110' variant='outline'><FaGithub />
+                </Button>
+                <Button className='rounded-full p-2 hover:bg-slate-300 hover:scale-110' variant='outline'><FaGoogle/></Button>
+              </div>
              </div>
           </div>
         </div>
