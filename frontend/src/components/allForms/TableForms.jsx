ee1d5a15341ac0576e36fae7f6e5652ca8958b14
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-12-15 10:25:52 +0500

    table search form ui sorting icons ui

--- frontend/src/components/allForms/TableForms.jsx
+++ frontend/src/components/allForms/TableForms.jsx
@@ -18,18 +18,32 @@ import {
   PaginationPrevious,
 } from "@/components/ui/pagination";
 import { Link } from "react-router-dom";
+import { Input } from "../ui/input";
+import { ArrowDownNarrowWide, Search } from "lucide-react";
+import { useState } from "react";
 
 const TabelForms = ({ data }) => {
+  const [sortTitle, setSortTitle] = useState("asc");
+  const [sortDate, setSortDate] = useState("asc");
+
   return (
     <div className="w-full bg-white rounded-lg px-5 py-3 shadow-md">
+      <div className="relative">
+        <Input placeholder="Search" className='w-1/2 pl-5 mx-2 my-4 px-10 outline-none' />
+        <Search className="absolute top-2 left-4 size-5 text-gray-400" />
+      </div>
       <Table>
         <TableCaption>A list of your all templates.</TableCaption>
         <TableHeader>
           <TableRow>
             <TableHead className="w-[50px]">ID</TableHead>
-            <TableHead>Title</TableHead>
+            <TableHead 
+             onClick={() => setSortTitle(sortTitle === "asc" ? "desc" : "asc")}
+            className="flex gap-3 items-center justify-start cursor-pointer"	>Title <ArrowDownNarrowWide className={`size-4 text-gray-500 ${sortTitle === "desc" ? "rotate-180" : ""}`} /> </TableHead>
             <TableHead>Status</TableHead>
-            <TableHead>Date</TableHead>
+            <TableHead
+            onClick={() => setSortDate(sortDate === "asc" ? "desc" : "asc")}
+            className="flex gap-3 items-center justify-start cursor-pointer">Date <ArrowDownNarrowWide className={`size-4 text-gray-500 ${sortDate === "desc" ? "rotate-180" : ""}`}  /></TableHead>
             <TableHead>Description</TableHead>
           </TableRow>
         </TableHeader>
