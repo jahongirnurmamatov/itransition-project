6cf2938d4757b08a4fa7ab60f4eb061777710665
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2024-12-18 13:17:39 +0500

    usetemplateStore zustand state management

---
+++ frontend/src/store/templateStore.js
@@ -0,0 +1,35 @@
+import axiosInstance from '@/lib/axiosInstance';
+import {create} from 'zustand'
+
+export const useTemplateStore = create((set) => ({
+    isLoading:false,
+    forms: [],
+    title: '',
+    topic: '',
+    image: '',
+    template: null,
+    setTemplate: (template) => set({ template }),
+    setForms: (forms) => set({ forms }),
+    setTitle: (title) => set({ title }),
+    setTopic: (topic) => set({ topic }),
+    setImage: (image) => set({ image }),
+
+    createTemplate: () => {
+        const template = {
+            title,
+            topic,
+            image,
+            forms,
+        };
+        try {
+            set({isLoading:true, error:null});
+            const res = axiosInstance.post('/template/create-template', template);
+            set({template:res.data.template, isLoading:false});
+        } catch (error) {
+            set({error:error.response.data.message, isLoading:false});
+            throw Error(error);
+        }
+        
+    },
+
+}))
