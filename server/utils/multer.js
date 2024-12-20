e9eff4a07ebe3f538bd4bcb562cd53a7ffc7b78a
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2024-12-20 18:20:02 +0500

    multer file limit and file check

--- server/utils/multer.js
+++ server/utils/multer.js
@@ -8,7 +8,23 @@ const storage = multer.diskStorage({
     }
 });
 
-const upload = multer({storage});
+const upload = multer({
+    storage,
+    limits: { fileSize: 10000000 },
+    fileFilter: (req, file, cb) => {
+        checkFileType(file, cb);
+    }
+});
 
+function checkFileType(file, cb) {
+    const filetypes = /jpeg|jpg|png|gif/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
 
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images Only!');
+    }
+}
  export default upload;
