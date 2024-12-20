908fe464c9a4c5bddbaee4add52822bb9ed88b00
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2024-12-20 18:18:35 +0500

    multer integration

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import uploadToCloudinary from "./utils/cloudinaryConfig.js";
+import upload from "./utils/multer.js";
 dotenv.config();
 
 const app = express();
@@ -12,6 +14,18 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+
+
+
+app.post('/upload', upload.single('file'), async (req, res) => {
+  try {
+    res.json('multer called')
+  
+  } catch (error) {
+    res.status(500).send('Error uploading file.');
+  }
+});
+
 
 // routes
 app.use("/api/auth", authRoute);
