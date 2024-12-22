2a4bb97344ac8839b728ca4774b8c5495e2cb29e
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2024-12-22 10:27:58 +0500

    create comment controller function

--- server/controller/comment.controller.js
+++ server/controller/comment.controller.js
@@ -1,5 +1,45 @@
+import prisma from "../db/prisma.js";
 
+export const addComment = async (req, res) => {
+  try {
+    const { content } = req.body; 
+    const { templateId } = req.params;
+    const userId = req.userId;
 
-export const addComment = async(req, res) => {
-    
-}
+    if (!content || content.trim() === "") {
+      return res.status(400).json({
+        success: false,
+        message: "Comment content cannot be empty.",
+      });
+    }
+
+    const template = await prisma.template.findUnique({
+      where: { id: parseInt(templateId) },
+    });
+    if (!template) {
+      return res.status(404).json({
+        success: false,
+        message: "Template not found.",
+      });
+    }
+
+    const newComment = await prisma.comment.create({
+      data: {
+        userId,
+        templateId: parseInt(templateId),
+        content,
+      },
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Comment added successfully.",
+      comment: newComment,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- server/routes/comment.route.js
+++ server/routes/comment.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { addComment } from '../controller/comment.controller.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
-router.post('/add-comment', addComment);
+router.post('/:templateId/add-comment',verifyToken, addComment);
 
 export default router;
