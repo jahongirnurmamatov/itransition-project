c7d1ca05ec80edeec7152ba02c68753d48fd7aa6
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2024-12-24 09:37:12 +0500

    verify if the user is admin

---
+++ server/middleware/verifyAdmin.js
@@ -0,0 +1,14 @@
+import prisma from "../db/prisma.js";
+
+export const verifyAdmin = async(req, res, next) => {  
+    try {
+        // Check if the user is an admin
+        const   currentUser = await prisma.user.findUnique({ where: { id: req.userId } });
+        if (currentUser.role !== "admin") {
+            return res.status(403).json({ success: false, message: "Unauthorized, you are not an admin" });
+        }
+        next();
+    } catch (error) {
+       res.status(500).json({ success: false, message: error.message }); 
+    }
+}
