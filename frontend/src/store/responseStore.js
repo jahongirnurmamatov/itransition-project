a3f567fd45bf782441d1d3557d5c7c9a495d02aa
jahongiir.nurmamatov <zokir.almatov1414@gmail.com>
2024-12-25 19:56:36 +0500

    response store

---
+++ frontend/src/store/responseStore.js
@@ -0,0 +1,20 @@
+import axiosInstance from '@/lib/axiosInstance.js';
+import {create} from 'zustand';
+
+export const useResponseStore = create((set) => ({
+    response: [],
+    isLoading: false,
+    error: null,
+    isAddingResponse: false,
+    responseError: null,
+    addResponse: async (templateId, answers) => {
+        try {
+            set({ isAddingResponse: true, responseError: null });
+            const res = await axiosInstance.post(`/response/${templateId}/submit`, {answers }); 
+            set({ response: res.data.response, isAddingResponse: false });    
+        } catch (error) {
+            set({ responseError: error.response.data.error, isAddingResponse: false });
+            throw Error(error);     
+        }
+    }
+}));
